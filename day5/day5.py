876f475c9ff3288e9ae1504d1199f36adb23beb9
piere05 <piereshc@gmail.com>
2026-02-10 18:27:01 +0530

    as of day 5

---
+++ day5/day5.py
@@ -0,0 +1,22 @@
+# word ="Iam Piere,What is your name"
+# sentence= " ".join(word)
+# print(sentence)
+
+# eplitw=word.split(",")
+# print(eplitw)
+
+# find len
+# data = "applebanana orange"
+# fruits = data.split(" ")
+# print(len(fruits))
+
+# filename= open("test.txt","r")
+
+with open("test.txt","w") as file:
+    file.write("Hello, iam Piere")
+    file.write("\nStill wonderning  who is this")
+
+
+with open("test.txt","r") as readd:
+    data=readd.read()
+    print(data)
