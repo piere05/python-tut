21514510c1a988d4b99dcb60ab759ea52c84e787
piere05 <piereshc@gmail.com>
2026-02-10 13:57:41 +0530

    feat: day 4 completed

---
+++ day4/day4.py
@@ -0,0 +1,20 @@
+#list of number
+# num = [1,2,1,3,1,2,4,4,3,5,6,5]
+# print(set(num))
+
+
+#list of strin
+
+# name= ["Piere" ,"Amal", "Raj", "Ram"]
+
+# result={w: len(w) for w in name}
+
+# print(result)
+
+
+#odd number 
+
+
+odd_number=[ x for x in range(1,51) if x%2!=0]
+
+print(odd_number)
