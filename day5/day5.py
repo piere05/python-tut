3af2f6450c8d319fb674e574bf5504298dc04111
piere05 <piereshc@gmail.com>
2026-02-11 13:17:09 +0530

    as of day5

--- day5/day5.py
+++ day5/day5.py
@@ -12,11 +12,24 @@
 
 # filename= open("test.txt","r")
 
-with open("test.txt","w") as file:
-    file.write("Hello, iam Piere")
-    file.write("\nStill wonderning  who is this")
+# with open("test.txt","w") as file:
+#     file.write("Hello, iam Piere")
+#     file.write("\nStill wonderning  who is this")
 
 
-with open("test.txt","r") as readd:
-    data=readd.read()
-    print(data)
+# with open("test.txt","r") as readd:
+#     data=readd.read()
+#     print(data)
+
+
+# text ="Python"
+
+# print(text.index("P")) 
+
+# num = int(input("Enter a Number :  "))
+# fact=1
+# for i in range(1, num+1):
+#     fact*=i
+# print(fact)
+
+
