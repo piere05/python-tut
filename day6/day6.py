8adbe241ca8d5c96d650d178ee275ad80981dcb8
piere05 <piereshc@gmail.com>
2026-02-13 10:12:38 +0530

    day 6 changes

--- day6/day6.py
+++ day6/day6.py
@@ -1,8 +1,22 @@
-class car:
-    def __init__(self,name):
-        self.name=name
+# class car:
+#     def __init__(self,name):
+#         self.name=name
 
-car1 = car("TATA")
-car2 = car("Renout")
-print(car1.name)
-print(car2.name)
+# car1 = car("TATA")
+# car2 = car("Renout")
+# print(car1.name)
+# print(car2.name)
+
+
+class InsufficientBalanceError(Exception):
+    pass
+
+
+bal = 1000
+amt =1100
+
+try:
+    if amt > bal:
+        raise InsufficientBalanceError("Not Sufficient Balance")
+except InsufficientBalanceError as e:
+    print(e)
